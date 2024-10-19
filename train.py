6bbbfd05cc03bc95215225538b89106cf04c9bd1
jomon <pjomon79@gmail.com>
2024-10-19 09:48:52 +0200

    Initial commit

---
+++ train.py
@@ -0,0 +1,99 @@
+import cv2
+import os
+from glob import glob
+import tensorflow as tf
+from unet import unet
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import Recall, Precision
+from metrices import dice_loss, dice_coef, iou
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, EarlyStopping, TensorBoard
+import numpy as np
+H = 512
+W = 512
+
+def load_dataset(path):
+    image_path_x = sorted(glob(os.path.join(path, "images", "*.jpg")))
+    mask_path_y = sorted(glob(os.path.join(path, "mask", "*.png")))
+    return image_path_x, mask_path_y
+
+def read_image(image_path): #read image
+    image_path = image_path.decode()
+
+    x = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    x = cv2.resize(x, (W, H))
+    x = x.astype("float32")
+
+    x = x / 255
+    return x
+
+def read_mask(mask_path): #read mask
+    mask_path = mask_path.decode()
+    x = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE) 
+    x = cv2.resize(x, (W, H))
+    x = x.astype("float32")
+
+    x = x / 255
+    x = np.expand_dims(x, axis=-1)              ## (512, 512, 1)
+
+    return x
+
+def tf_parse(image_path, mask_path):
+    def _parse(image_path, mask_path):
+        x = read_image(image_path)
+        y = read_mask(mask_path)
+        return x,y
+    x, y = tf.numpy_function(_parse, (image_path, mask_path), (tf.float32, tf.float32))
+    #x.set_shape([H, W, 3])
+    #y.set_shape([H, W, 1])
+    return x, y
+
+def tf_dataset(image_path, mask_path, batch_size):
+    dataset = tf.data.Dataset.from_tensor_slices((image_path, mask_path))
+    dataset = dataset.map(tf_parse)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(4)
+    return dataset
+
+
+    
+if __name__ ==  "__main__":
+
+    batch_size = 1
+    lr = 1e-4
+
+    num_epochs = 20
+
+    train_data_path = "/home/ec2-user/kitchen/polygon/train/"
+    valid_data_path = "/home/ec2-user/kitchen/polygon/test/"
+    train_x, train_y = load_dataset(train_data_path) #loading dataset path
+    valid_x, valid_y = load_dataset(valid_data_path)
+    train_dataset = tf_dataset(train_x, train_y, batch_size)
+    valid_dataset = tf_dataset(valid_x, valid_y, batch_size)
+
+    train_step = len(train_x) // batch_size
+    valid_step = len(valid_x) //batch_size
+
+    if len(train_x) % batch_size != 0:
+        train_step += 1
+    if len(valid_x) % batch_size != 0:
+        valid_step += 1
+
+    model = unet((H, W, 3))
+    model.compile(loss=dice_loss, optimizer=Adam(lr), metrics=[dice_coef, iou, Recall(), Precision()])
+
+    callbacks = [
+        ModelCheckpoint(model_path, verbose=1, save_best_only=True),
+        ReduceLROnPlateau(monitor="val_loss", factor=0.1, patience=5, min_lr=1e-6, verbose=1),
+        TensorBoard(),EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=False)
+                ]
+
+    model.fit(
+        train_dataset,
+        epochs=num_epochs,
+        validation_data=valid_dataset,
+        steps_per_epoch=train_step,
+        validation_steps=valid_step,
+        callbacks=callbacks
+    ) 
+
+    model_path = "/home/ec2-user/model.h5"
---
+++ unet.py
@@ -0,0 +1,97 @@
+import tensorflow as tf 
+import numpy as np 
+from PIL import Image 
+from tensorflow.keras.preprocessing import image 
+
+def convol_relu(input, num_filters):
+    x = tf.keras.layers.Conv2D(num_filters, (3,3), padding = 'same')(input)
+    x = tf.keras.layers.Activation("relu")(x)
+    x = tf.keras.layers.Conv2D(num_filters, (3,3), padding = 'same')(x)
+    x = tf.keras.layers.Activation("relu")(x)
+    return x
+
+def maxpool(input):
+    x = tf.keras.layers.MaxPool2D((2,2))(input)
+    return x
+    
+def up_conv(input, num_filters):
+    x = tf.keras.layers.Conv2DTranspose(num_filters, (2,2), strides = 2, padding = "same")(input)
+    #x = convol_relu(x, num_filters)
+    return x
+
+def concatination(decoder_val, encoder_val):
+    encoder_val = tf.image.resize(encoder_val, size = (decoder_val.shape[1],decoder_val.shape[2])) 
+    concat_img = tf.keras.layers.Concatenate()([decoder_val, encoder_val])
+    return concat_img
+
+
+def unet(input_shape):
+    input = tf.keras.layers.Input(input_shape) 
+    enc1 = convol_relu(input, 64)
+    enc1 = maxpool(enc1)
+
+    enc2 = convol_relu(enc1, 128)
+    enc2 = maxpool(enc2)
+
+    enc3 = convol_relu(enc2, 256)
+    enc3 = maxpool(enc3)
+
+    enc4 = convol_relu(enc3, 512)
+    enc4 = maxpool(enc4)
+
+    bridge_in = convol_relu(enc4, 1024)
+    bridge_out = up_conv(bridge_in, 1024)
+    bridge_out = concatination(bridge_out, enc4)
+
+    dec1 = convol_relu(bridge_out, 512)
+    dec1 = up_conv(dec1, 512)
+    dec1 = concatination(dec1, enc3)
+
+    dec2 = convol_relu(dec1, 256)
+    dec2 = up_conv(dec2, 256)
+    dec2 = concatination(dec2, enc2)
+
+    dec3 = convol_relu(dec2, 128)
+    dec3 = up_conv(dec3, 128)
+    dec3 = concatination(dec3, enc1)
+
+    output = convol_relu(dec3, 64)
+    output = tf.keras.layers.Conv2D(1, 1)(output)
+
+    model = tf.keras.models.Model(inputs = input, outputs = output, name = 'U-Net') 
+    return model
+
+if __name__ == '__main__': 
+    model = unet(input_shape=(512, 512, 3)) 
+    model.summary()
+'''
+img = Image.open('cat.png') 
+# Preprocess the image 
+img = img.resize((572, 572)) 
+img_array = image.img_to_array(img) 
+img_array = np.expand_dims(img_array[:,:,:3], axis=0) 
+img_array = img_array / 255
+  
+# Load the model 
+model = unet(input_shape=(572, 572, 3)) 
+  
+# Make predictions 
+predictions = model.predict(img_array) 
+  
+# Convert predictions to a numpy array and resize to original image size 
+predictions = np.squeeze(predictions, axis=0) 
+predictions = np.argmax(predictions, axis=-1) 
+predictions = Image.fromarray(np.uint8(predictions*255)) 
+predictions = predictions.resize((img.width, img.height)) 
+  
+# Save the predicted image 
+predictions.save('predicted_image.jpg') 
+predictions
+
+'''
+
+
+
+
+
+
